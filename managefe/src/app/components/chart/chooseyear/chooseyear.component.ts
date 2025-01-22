6f203643457f7e55abc9ee3eae521c0dc66e1674
khanhleg2003 <lythatda942003@gmail.com>
2025-01-22 17:12:28 +0700

    2 chart fe

---
+++ managefe/src/app/components/chart/chooseyear/chooseyear.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chooseyear',
+  standalone: true,
+  imports: [],
+  templateUrl: './chooseyear.component.html',
+  styleUrl: './chooseyear.component.css'
+})
+export class ChooseyearComponent {
+
+}
--- managefe/src/app/layout2/main-layout/main/main.component.ts
+++ managefe/src/app/layout2/main-layout/main/main.component.ts
@@ -4,10 +4,11 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { ChartModule } from 'primeng/chart';
 import { CountryChartComponent } from '../../../components/chart/country-chart/country-chart.component';
 import { InvtChartComponent } from '../../../components/chart/invt-chart/invt-chart.component';
+import { ChooseyearComponent } from "../../../components/chart/chooseyear/chooseyear.component";
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterModule, ChartModule, CountryChartComponent, InvtChartComponent],
+  imports: [RouterModule, ChartModule, CountryChartComponent, InvtChartComponent, ChooseyearComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
